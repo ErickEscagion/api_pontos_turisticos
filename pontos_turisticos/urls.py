6bee24187ab5d6e63890df52dfec42d5e8682fcd
Erick Escagion <erickeescagion@gmail.com>
2021-01-07 21:17:50 -0300

    criado rota "pontoturistico" da api

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+from core.api.viewsets import PontoTuristicoViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'pontoturistico', PontoTuristicoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include(router.urls))
 ]
